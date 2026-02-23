fff8a11a2f05474f3ff8b99f147915e54bea6814
Ananya Chakraborty Gourab <aborty174@gmail.com>
2026-02-23 23:44:14 +0600

    working for interview rejected button

--- script/script.js
+++ script/script.js
@@ -44,33 +44,79 @@ function toggleStyle(id){
 
     selected.classList.remove('bg-gray-300','text:blue')
     selected.classList.add('bg-black','text-white');
+
+    if(id=='interview-filter-btn'){
+        addCardButton.classList.add('hidden');
+
+        filterSection.classList.remove('hidden');
+    }else if(id=='all-filter-btn'){
+        allCardSection.classList.remove('hidden');
+        filterSection.classList.add('hidden');
+    }
 }
 
 mainContainer.addEventListener('click',function(event){
 
+   // console.log(event,event.target.parentNode.parentNode);
+
+    //console.log(event.target.classList.contains('interview-filter-btn'));
+
     if(event.target.classList.contains('interview-btn')){
       const parentNode=event.target.parentNode.parentNode;
-    const mobileFirst=parentNode.querySelector('.mobileFirst');
-    const reactNative=parentNode.querySelector('.reactNative');
-    const remote=parentNode.querySelector('.remote');
-    const status=parentNode.querySelector('.status');
-    const notes=parentNode.querySelector('.notes');
 
+    const mobileFirst=parentNode.querySelector('.mobileFirst').innerText
+    const reactNative=parentNode.querySelector('.reactNative').innerText
+    const remote=parentNode.querySelector('.remote').innerText
+    const status=parentNode.querySelector('.status').innerText
+    const notes=parentNode.querySelector('.notes').innerText
+    
+    parentNode.querySelector('.status').innerText='interview'
 
     const cardInfo={
         mobileFirst,
         reactNative,
         remote,
-        status,
+        status:'interview',
         notes
     }
      //console.log(cardInfo);
 
      const mobileExist=interviewList.find(item=>item.mobileFirst==cardInfo.mobileFirst);
-
+     
+    // parentNode.querySelector('.status').innerText='interview'
      if(!mobileExist){
         interviewList.push(cardInfo);
      }
+     calculateCount();
+     renderInterview();
+    }
+    else if(event.target.classList.contains('rejected-btn')){
+      const parentNode=event.target.parentNode.parentNode;
+
+    const mobileFirst=parentNode.querySelector('.mobileFirst').innerText
+    const reactNative=parentNode.querySelector('.reactNative').innerText
+    const remote=parentNode.querySelector('.remote').innerText
+    const status=parentNode.querySelector('.status').innerText
+    const notes=parentNode.querySelector('.notes').innerText
+    
+    parentNode.querySelector('.status').innerText='rejected'
+
+    const cardInfo={
+        mobileFirst,
+        reactNative,
+        remote,
+        status:'rejected',
+        notes
+    }
+     //console.log(cardInfo);
+
+     const mobileExist=rejectedList.find(item=>item.mobileFirst==cardInfo.mobileFirst);
+     
+    // parentNode.querySelector('.status').innerText='interview'
+     if(!mobileExist){
+        rejectedList.push(cardInfo);
+     }
+     calculateCount();
      renderInterview();
     }
 })
@@ -83,11 +129,11 @@ function renderInterview(){
     for(let interview of interviewList){
         let div=document.createElement('div');
         div.className='card flex justify-between border p-8'
-        div.innerText=`
+        div.innerHTML=`
                       <div class="space-y-6">
             <!--part 1-->
             <div>
-                <p class="mobileFirst text-4xl">Mobile First Crop</p>
+                <p class="mobileFirst text-4xl">${interview.mobileFirst}</p>
                 <p class="reactNative">react Native Developer</p>
             </div>
 
@@ -97,7 +143,7 @@ function renderInterview(){
              </div>
 
              <!--part 3-->
-                <p class="status bg-gray-200 px-5">NOT APPLIED</p>
+                <p class="status bg-gray-200 px-5">${interview.status}</p>
                 <p class="notes">Build cross-platform mobile applications using React Native. Work on products used by millions of users worldwide.</p>
 
                 <div class="flex gap-5">
@@ -111,5 +157,44 @@ function renderInterview(){
         </div>
       </div>
         `
+        filterSection.appendChild(div);
+    }
+    //calculateCount();
+}
+    function renderRejected(){
+    filterSection.innerHTML=''
+
+    for(let rejected of rejectedList){
+        let div=document.createElement('div');
+        div.className='card flex justify-between border p-8'
+        div.innerHTML=`
+                      <div class="space-y-6">
+            <!--part 1-->
+            <div>
+                <p class="mobileFirst text-4xl">${rejected.mobileFirst}</p>
+                <p class="reactNative">react Native Developer</p>
+            </div>
+
+             <!--part 2-->
+             <div>
+                <p class="remote">Remote• Full-time • $130,000 - $175,000</p>
+             </div>
+
+             <!--part 3-->
+                <p class="status bg-gray-200 px-5">${rejected.status}</p>
+                <p class="notes">Build cross-platform mobile applications using React Native. Work on products used by millions of users worldwide.</p>
+
+                <div class="flex gap-5">
+                    <button class="interview-btn bg-gray-100 text-green-400">INTERVIEW</button>
+                    <button class="rejected-btn bg-gray-100 text-red-400">REJECTED</button>
+                </div>
+        </div>
+        <!--main part 2-->
+        <div>
+            <button class="btn-delete"><img src="./Group 1.png" alt=""></button>
+        </div>
+      </div>
+        `
+        filterSection.appendChild(div);
     }
 }
